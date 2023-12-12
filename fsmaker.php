87200ebe97c9f10e36d915b447ab434df629b956
Antonio <elguitarraverde@hotmail.com>
2023-12-12 12:00:20 +0100

    fix .gitignore en carpetas vacias

--- fsmaker.php
+++ fsmaker.php
@@ -739,7 +739,7 @@ final class fsmaker
         ];
         foreach ($folders as $folder) {
             $this->createFolder($name . '/' . $folder);
-            file_put_contents($name . '/' . $folder . '/.gitignore', "*\n!.gitignore");
+            touch($name . '/' . $folder . '/.gitignore');
         }
 
         foreach (explode(',', self::TRANSLATIONS) as $filename) {
