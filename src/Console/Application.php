75ebde1567484c975efb2f595f165827aee94810
Carlos Garcia <neorazorx@gmail.com>
2026-03-20 17:56:48 +0100

    Bump version to 2.2.0 and rename app to fsmaker

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -22,7 +22,7 @@ class Application extends BaseApplication
 {
     public function __construct()
     {
-        parent::__construct('FacturaScripts Maker', '2.1.0');
+        parent::__construct('fsmaker', '2.2.0');
         $this->addCommands($this->getCommands());
     }
 
